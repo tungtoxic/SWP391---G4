f698c50a00b433abf437fd2eaec6a3a21e90e95b
Thanh2004-coder <thanh200417@gmail.com>
2025-10-09 13:05:42 +0700

    Create ProductCategory.java

---
+++ src/java/entity/ProductCategory.java
@@ -0,0 +1,17 @@
+package entity;
+
+public class ProductCategory {
+    private int categoryId;
+    private String categoryName;
+    private String description;
+
+    // Getter/Setter
+    public int getCategoryId() { return categoryId; }
+    public void setCategoryId(int categoryId) { this.categoryId = categoryId; }
+
+    public String getCategoryName() { return categoryName; }
+    public void setCategoryName(String categoryName) { this.categoryName = categoryName; }
+
+    public String getDescription() { return description; }
+    public void setDescription(String description) { this.description = description; }
+}
